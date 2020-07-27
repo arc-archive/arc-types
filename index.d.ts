c96568177253bbe63a26a425109a15035fd9e339
Pawel <jarrodek@gmail.com>
2020-07-27 16:15:46 -0700

    fix: adding outh2 export

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,9 @@
 import * as Cookies from './src/cookies/Cookies';
 import * as DataExport from './src/dataexport/DataExport';
+import * as OAuth2 from './src/oauth2/OAuth2';
 
 export {
   DataExport,
   Cookies,
+  OAuth2,
 };
