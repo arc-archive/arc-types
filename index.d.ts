c45fc9399dfe8e99cafda46c8f00f0a3b6c5e5f3
Pawel <jarrodek@gmail.com>
2020-07-27 17:30:11 -0700

    feat: adding Google Drive types

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,11 @@
 import * as Cookies from './src/cookies/Cookies';
 import * as DataExport from './src/dataexport/DataExport';
 import * as OAuth2 from './src/oauth2/OAuth2';
+import * as GoogleDrive from './src/googledrive/GDrive';
 
 export {
   DataExport,
   Cookies,
   OAuth2,
+  GoogleDrive,
 };
