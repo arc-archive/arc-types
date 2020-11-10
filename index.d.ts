a2df736a89c6837a5dc32fcd8c4d0612c3f994ba
Pawel <jarrodek@gmail.com>
2020-11-09 20:44:15 -0800

    refactor: moving oauth 2 to Authorization
    Signed-off-by: Pawel <jarrodek@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,5 @@
 import * as Cookies from './src/cookies/Cookies';
 import * as DataExport from './src/dataexport/DataExport';
-import * as OAuth2 from './src/oauth2/OAuth2';
 import * as GoogleDrive from './src/googledrive/GDrive';
 import * as FormTypes from './src/forms/FormTypes';
 import * as ArcResponse from './src/request/ArcResponse';
--- src/authorization/Authorization.d.ts
+++ src/authorization/Authorization.d.ts
@@ -234,3 +234,78 @@ export declare interface OAuth2Authorization extends BaseOAuth2Authorization {
    */
   pkce?: boolean;
 }
+
+/**
+ * Options for removing the OAuth 2 token from the cache.
+ */
+export declare interface TokenRemoveOptions {
+  /**
+   * The client id used to issue the token.
+   */
+  clientId: string;
+  /**
+   * The authorization URI used to issue the token.
+   * For the request that do not have `authorizationUrl` a value for
+   * `accessTokenUri` should be used.
+   */
+  authorizationUri: string;
+}
+
+declare interface TokenBase {
+  /**
+   * Whether the token request was marked as interactive.
+   */
+  interactive?: boolean;
+  /**
+   * The request state parameter, if used with the request.
+   */
+  state: string;
+}
+
+/**
+ * OAuth 2 token response object.
+ */
+declare interface TokenInfo extends TokenBase {
+  /**
+   * The access token.
+   */
+  accessToken: string;
+  /**
+   * The access token type.
+   */
+  tokenType?: string;
+  /**
+   * Access token expiration timeout.
+   */
+  expiresIn: number;
+  /**
+   * Access token expiration timestamp
+   */
+  expiresAt: number;
+  /**
+   * When `true` the `expires_in` and `expires_at` are assumed values (1 hour).
+   */
+  expiresAssumed: boolean;
+  /**
+   * The list of scopes the token has been granted
+   */
+  scope?: string[];
+  /**
+   * The refresh token, when requested
+   */
+  refreshToken?: string;
+}
+
+/**
+ * Token response object.
+ */
+declare interface TokenError {
+  /**
+   * The error message
+   */
+  message: string;
+  /**
+   * One of the application error codes.
+   */
+  code: string;
+}
